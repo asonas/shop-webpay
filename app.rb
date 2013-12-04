f596f90cb0a294223b8ff8352a6aedcec160d4f9
asonas <hzw1258@gmail.com>
2013-12-05 04:40:48 +0900

    Goodby api key

--- app.rb
+++ app.rb
@@ -4,10 +4,12 @@ require "sinatra/reloader" if development?
 require "sinatra/activerecord"
 require "./environments"
 
+@config = YAML.load(open("./config.yml"))
+WebPay.api_key = @config["secret_key"]
+
 class Purchases < ActiveRecord::Base
 end
 
 get "/" do
-  @config = YAML.load(open("./config.yml"))
   slim :index
 end
