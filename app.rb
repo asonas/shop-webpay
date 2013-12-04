753da117bfd7f8dd8957927e5fc5efd8e64e9234
asonas <hzw1258@gmail.com>
2013-12-04 19:17:47 +0900

    wip: heroku

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
-require 'bundler'
-Bundler.require
+#require 'bundler'
+#Bundler.require
+require 'sinatra'
 require "sinatra/reloader" if development?
+require 'sinatra/activerecord'
+require "./environments"
 
+class Purchases < ActiveRecord::Base
+end
 
 get "/" do
   @config = YAML.load(open("./config.yml"))
